113894a84ad250931aba7fe176b0f6625444ffef
Valentyn Ponomarenko <>
2024-10-06 16:06:58 +0200

    Initial commit

---
+++ PetCareTracker/PetCareTrackerApp.swift
@@ -0,0 +1,16 @@
+//
+//  PetCareTrackerApp.swift
+//  PetCareTracker
+//
+//  Created by Valentyn Ponomarenko on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PetCareTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
