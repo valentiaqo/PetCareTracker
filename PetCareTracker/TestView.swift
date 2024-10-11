36f19253c81b1b36c44bb779c2cfbd79a29d0940
Valentyn Ponomarenko <>
2024-10-11 08:08:07 +0200

    Added linear and colored icons to the assets.

---
+++ PetCareTracker/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  PetCareTracker
+//
+//  Created by Valentyn Ponomarenko on 09/10/2024.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
